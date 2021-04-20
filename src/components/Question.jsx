a9ab632a383f062bc9d53151fcf2c66c4c5130df
code4fun907 <skylerak04@gmail.com>
2021-04-20 11:56:33 -0800

    Add ability to click on a tag from a question to add to filter tags

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -2,8 +2,11 @@ import Card from "./ui/Card";
 import InfoIcon from "./ui/InfoIcon";
 import HeartIcon from "../icons/HeartIcon";
 import CommentIcon from "../icons/CommentIcon";
+import { useSearch } from "../contexts/Search";
 
 const Question = ({ title, contentPreview, tags, likes, comments }) => {
+  const { setFilterTags } = useSearch();
+
   return (
     <Card className="p-4 break-words bg-white">
       <h1 className="mb-4 text-lg text-blue-600">{title}</h1>
@@ -25,7 +28,11 @@ const Question = ({ title, contentPreview, tags, likes, comments }) => {
         </div>
         <div className="flex items-start gap-4">
           {tags.map((tag, index) => (
-            <Card key={index} className="p-1">
+            <Card
+              onClick={() => setFilterTags((prev) => [...prev, tag])}
+              key={index}
+              className="p-1 cursor-pointer"
+            >
               {tag}
             </Card>
           ))}
--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,9 +1,10 @@
 import clsx from "clsx";
 
-const Card = ({ children, className }) => {
+const Card = ({ children, className, ...props }) => {
   return (
     <div
       className={clsx("border border-gray-200 shadow rounded-md", className)}
+      {...props}
     >
       {children}
     </div>
